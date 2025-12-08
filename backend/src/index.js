a6544a56e3f41977f2c6f1d7b340ccbdea352f29
Nasibullah Nasiri <nasibullah.nasiri@telefonica.com>
2025-12-08 12:43:40 +0100

    refactor

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { pool } from "./db.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 
@@ -11,66 +11,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/tasks", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM tasks ORDER BY id DESC");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-app.post("/tasks", async (req, res) => {
-  const { title, description } = req.body;
-  try {
-    const result = await pool.query(
-      "INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *",
-      [title, description]
-    );
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await pool.query(
-      "DELETE FROM tasks WHERE id = $1 RETURNING *",
-      [id]
-    );
-    if (result.rowCount === 0) {
-      return res.status(404).json({ error: "Task nicht gefunden" });
-    }
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-app.put("/tasks/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
-
-  try {
-    const result = await pool.query(
-      "UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *",
-      [title, description, id]
-    );
-
-    if (result.rowCount === 0) {
-      return res.status(404).json({ error: "Task nicht gefunden" });
-    }
-
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-});
+app.use("/tasks", taskRoutes);
 
 app.listen(PORT, () => console.log(`Backend läuft auf Port ${PORT}`));
