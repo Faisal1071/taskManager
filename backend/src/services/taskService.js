ce007b76c212c19106513e3b071f2941b74f0a5f
Nasibullah Nasiri <nasibullah.nasiri@telefonica.com>
2025-12-09 14:08:11 +0100

    add deadline as parameter || outsourced css

--- backend/src/services/taskService.js
+++ backend/src/services/taskService.js
@@ -4,11 +4,10 @@ export const getAllTasks = async () => {
   const result = await pool.query("SELECT * FROM tasks ORDER BY id DESC");
   return result.rows;
 };
-
-export const createTask = async (title, description) => {
+export const createTask = async (title, description, deadline) => {
   const result = await pool.query(
-    "INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *",
-    [title, description]
+    "INSERT INTO tasks (title, description, deadline) VALUES ($1, $2, $3) RETURNING *",
+    [title, description, deadline]
   );
   return result.rows[0];
 };
@@ -21,10 +20,10 @@ export const deleteTask = async (id) => {
   return result.rows[0];
 };
 
-export const updateTask = async (id, title, description) => {
+export const updateTask = async (id, title, description, deadline) => {
   const result = await pool.query(
-    "UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *",
-    [title, description, id]
+    "UPDATE tasks SET title = $1, description = $2, deadline = $3 WHERE id = $4 RETURNING *",
+    [title, description, deadline, id]
   );
   return result.rows[0];
 };
